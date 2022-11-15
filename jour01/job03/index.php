b03ba808b338a76ccda83d7b4a86564b8239f392
abraham-ukachi <abraham.ukachi@laplateforme.io>
2022-11-15 08:22:44 +0100

    Create jour01 folder

---
+++ jour01/job03/index.php
@@ -0,0 +1,33 @@
+<?php
+	/*
+	 * Name: 'Job 0.9'
+	 * Script: index.php
+	 * Author: Abraham Ukachi <abraham.ukachi@laplateforme.io>
+	 * Version: -
+	 * 
+	 * Usage:
+	 *		1-|> open http://localhost/runtrack2/jour01/job00/index.php
+	 *
+	 *
+	 * ======== Job 0.9 ==========
+	 * 			>>> DESCRIPTION <<<		
+	 * ~~~~~~~~ (French) ~~~~~~~~~
+	 *
+	 * 	-	Créer un fichier index.php qui affiche “Hello LaPlateforme!” 
+	 *		en php en utilisant echo. Ouvrez le avec votre navigateur préféré.
+	 *
+	 * ~~~~~~~~ (English) ~~~~~~~~
+	 * 
+	 * 	-	Create an index.php file that displays “Hello The Platform!” 
+	 *		in php using echo. Open it with your favorite browser.
+	 *
+	 * ============================
+	 */
+
+	/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	 * MOTTO: I'll always do more 😜 !!
+	 * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	 */
+	
+	echo phpinfo();
+?>

0a9869f0230fb11563ff47da981f963ccf495b4e
abraham-ukachi <abraham.ukachi@laplateforme.io>
2022-11-25 17:10:42 +0100

    Desc added w/ major typos fixed

--- jour09/job01.sql
+++ jour09/job01.sql
@@ -6,6 +6,91 @@
 -- Generation Time: Nov 25, 2022 at 11:49 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.6
+-- 
+-- ===============================================
+--
+-- Job: 01
+-- Day: 09 - (Thursday)
+-- Name: Jour 9 - Hello SQL
+-- File: job01.sql
+-- Author: Abraham Ukachi <abraham.ukachi@laplateforme.io>
+-- Version: 0.0.1
+--
+-- Usage:
+--  1-|> mysql jour09 < job01.sql > output.tab
+-- 
+--
+-- ======== Job 01 ==========
+--    >>> DESCRIPTION <<<		
+-- ~~~~~~~~ (French) ~~~~~~~~~
+-- 
+-- - A l’aide de phpmyadmin, créez une base de données nommée “jour09”. 
+--   Créez une table nommée “etudiants” ayant comme champs :
+--
+--    ● id, int, clé primaire et Auto Incrément
+--    ● prenom, varchar de taille 255
+--    ● nom, varchar de taille 255
+--    ● naissance, date
+--    ● sexe, varchar de taille 25
+--    ● email, varchar de taille 255
+--
+-- - Créez une table nommée “etages” ayant comme champs :
+--    ● id, int, clé primaire et Auto Incrément
+--    ● nom, varchar de taille 255
+--    ● numero, int
+--    ● superficie, int
+--
+-- - Créez une table nommée “salles” ayant comme champs :
+--    ● id, int, clé primaire et Auto Incrément
+--    ● nom, varchar de taille 255
+--    ● id_etage, int
+--    ● capacite, int
+--
+-- - Exportez votre base de données en utilisant la méthode d’exportation rapide, 
+--   renommez là "job 01.sql" et ajoutez la à votre répertoire de rendu.
+--
+-- ~~~~~~~~ (English) ~~~~~~~~
+-- 
+-- - Using phpmyadmin, create a database named “jour09”. Create a table named “students” 
+--   having as fields:
+--
+--    ● id, int, primary key and Auto Increment
+--    ● firstname, varchar of size 255
+--    ● name, varchar of size 255
+--    ● birth, date
+--    ● sex, size 25 varchar
+--    ● email, varchar of size 255
+--
+--
+-- - Create a table named "floors" having as fields:
+--
+--    ● id, int, primary key and Auto Increment
+--    ● name, varchar of size 255
+--    ● number, int
+--    ● area, int
+--
+-- - Create a table named "rooms" having as fields:
+--
+--    ● id, int, primary key and Auto Increment
+--    ● name, varchar of size 255
+--    ● floor, int
+--    ● capacity, int
+--
+-- - Export your database using the quick export method, rename it 
+--   "job 01.sql" and add it to your render directory.
+--
+-- ============================
+-- IMPORTANT:  
+-- ============================
+--
+--
+--
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- MOTTO: I'll always do more 😜!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+--
+--
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,14 +105,16 @@ SET time_zone = "+00:00";
 --
 -- Database: `jour09`
 --
+CREATE DATABASE IF NOT EXISTS `jour09` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4 utf8mb4_general_ci;
+USE `jour09`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `etage`
+-- Table structure for table `etages`
 --
 
-CREATE TABLE `etage` (
+CREATE TABLE `etages` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `numero` int(11) DEFAULT NULL,
@@ -58,7 +145,7 @@ CREATE TABLE `etudiants` (
 CREATE TABLE `salles` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) DEFAULT NULL,
-  `id_etage` int(11) DEFAULT NULL,
+  `etage` int(11) DEFAULT NULL,
   `capacite` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -67,9 +154,9 @@ CREATE TABLE `salles` (
 --
 
 --
--- Indexes for table `etage`
+-- Indexes for table `etages`
 --
-ALTER TABLE `etage`
+ALTER TABLE `etages`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -89,9 +176,9 @@ ALTER TABLE `salles`
 --
 
 --
--- AUTO_INCREMENT for table `etage`
+-- AUTO_INCREMENT for table `etages`
 --
-ALTER TABLE `etage`
+ALTER TABLE `etages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
